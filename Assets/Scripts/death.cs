62fac920500ac2732054a684669c2aef526d7036
WESLEY V GERBER <u1166683@ad.utah.edu>
2018-09-21 20:17:04 -0600

    added player and obsticle

---
+++ Assets/Scripts/death.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class death : MonoBehaviour {
+
+    public static event Action PlayerDeathEvent;
+
+    void OnCollisionEnter(Collision  collision)
+    {
+        if (collision.gameObject.tag == "obstacle")
+        {
+            function1();
+            function2();
+
+        }
+    }
+
+    void function1()
+    {
+
+    }
+    void function2()
+    {
+
+    }
+}
