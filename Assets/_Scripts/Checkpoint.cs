eeeed406e9d1431a44e6ca9f1f23a180fd285a7f
oliverpgallina <oliverpgallina@gmail.com>
2018-09-21 20:02:05 -0600

    checkpoint trigger

---
+++ Assets/_Scripts/Checkpoint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour {
+
+    public static event Action CheckpointAction;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        CheckpointAction.Invoke();
+    }
+
+    // Update is called once per frame
+    void Update () {
+	}
+}
